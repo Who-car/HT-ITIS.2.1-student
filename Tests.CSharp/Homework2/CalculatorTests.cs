5464f75b40420ae8f31d4daf539b2c8c5a526f01
GaliullinMihail <55558861+GaliullinMihail@users.noreply.github.com>
2023-09-20 13:05:47 +0300

    Revert "ДЗ 2. Реализация методов Parse и Calculate на IL коде"

--- Tests.CSharp/Homework2/CalculatorTests.cs
+++ Tests.CSharp/Homework2/CalculatorTests.cs
@@ -12,49 +12,30 @@ public class CalculatorTests
     [InlineData(15, 5, CalculatorOperation.Divide, 3)]
     public void TestAllOperations(int value1, int value2, CalculatorOperation operation, int expectedValue)
     {
-        //arrange
-        
-        //act
-        var result = Hw2.Calculator.Calculate(value1, operation, value2);
-
-        //assert
-        Assert.Equal(expectedValue, result);
+        throw new NotImplementedException();
     }
 
     [Homework(Homeworks.HomeWork2)]
     public void TestInvalidOperation()
     {
-        //assert
-        Assert.Throws<ArgumentOutOfRangeException>(() => Calculator.Calculate(0, CalculatorOperation.Undefined, 10));
+        throw new NotImplementedException();
     }
 
     [Homework(Homeworks.HomeWork2)]
     public void TestDividingNonZeroByZero()
     {
-        //act
-        var actual = Calculator.Calculate(0, CalculatorOperation.Divide, 10);
-
-        //assert
-        Assert.Equal(0, actual);
+        throw new NotImplementedException();
     }
 
     [Homework(Homeworks.HomeWork2)]
     public void TestDividingZeroByNonZero()
     {
-        //act
-        var actual = Calculator.Calculate(10, CalculatorOperation.Divide, 0);
-
-        //assert
-        Assert.Equal(double.PositiveInfinity, actual);
+        throw new NotImplementedException();
     }
 
     [Homework(Homeworks.HomeWork2)]
     public void TestDividingZeroByZero()
     {
-        //act
-        var actual = Calculator.Calculate(0, CalculatorOperation.Divide, 0);
-
-        //assert
-        Assert.Equal(double.NaN, actual);
+        throw new NotImplementedException();
     }
 }
