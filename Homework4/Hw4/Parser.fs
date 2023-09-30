8eb0b4defff81668bbe720d16e5e8bbf4c01363f
Who-car <aysulu2645@gmail.com>
2023-10-01 00:16:03 +0300

    ДЗ 4. Калькулятор на F#

--- Homework4/Hw4/Parser.fs
+++ Homework4/Hw4/Parser.fs
@@ -3,7 +3,6 @@
 open System
 open Hw4.Calculator
 
-
 type CalcOptions = {
     arg1: float
     arg2: float
@@ -11,10 +10,26 @@ type CalcOptions = {
 }
 
 let isArgLengthSupported (args : string[]) =
-    NotImplementedException() |> raise
+    args.Length = 3
 
 let parseOperation (arg : string) =
-    NotImplementedException() |> raise
+    match arg with
+    | "+" -> CalculatorOperation.Plus
+    | "-" -> CalculatorOperation.Minus
+    | "*" -> CalculatorOperation.Multiply
+    | "/" -> CalculatorOperation.Divide
+    | _ -> InvalidOperationException "Операция не распознана. Вводите только +, -, *, /" |> raise
+    
+let parseNum (arg: string) =
+    let mutable result = 0.0
+    if Double.TryParse(arg, &result) then result
+    else ArgumentException "Пожалуйста используйте только цифры" |> raise
+        
     
 let parseCalcArguments(args : string[]) =
-    NotImplementedException() |> raise
+    if isArgLengthSupported args then {
+        arg1 = parseNum args[0]
+        operation =  parseOperation args[1]
+        arg2 = parseNum args[2]
+    }
+    else ArgumentException "Неверное количество аргументов. Введите число, одну из операций +, -, *, /, число" |> raise
