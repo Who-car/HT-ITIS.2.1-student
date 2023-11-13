b94d83e2914c987a7a65a6614afe2b19a1cb6d89
Who-car <aysulu2645@gmail.com>
2023-11-13 22:41:58 +0300

    ДЗ 9. Реализован калькулятор с использованием деревьев выражений

---
+++ Homework9/Hw9/Services/MathCalculator/ParallelEvaluationVisitor.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq.Expressions;
+using Hw9.ErrorMessages;
+
+namespace Hw9.Services.MathCalculator;
+
+public class ParallelEvaluationVisitor : ExpressionVisitor
+{
+    public static Task<Expression> VisitExpression(Expression expression)
+    {
+        return Task.Run(() => new ParallelEvaluationVisitor().Visit(expression));
+    }
+    
+    protected override Expression VisitBinary(BinaryExpression node)
+    {
+        return Expression.Constant(VisitBinaryAsync(node).Result);
+    }
+
+    private async Task<Expression> VisitBinaryAsync(BinaryExpression node)
+    {
+        var firstTask = new Lazy<Task<ConstantExpression>>(async () =>
+        {
+            await Task.Delay(1000);
+            if (node.Left is BinaryExpression binaryLeft)
+                return Expression.Constant(await VisitBinaryAsync(binaryLeft));
+            return Expression.Constant(node.Left);
+        });
+        var secondTask = new Lazy<Task<ConstantExpression>>(async () =>
+        {
+            await Task.Delay(1000);
+            if (node.Right is BinaryExpression binaryLeft)
+                return Expression.Constant(await VisitBinaryAsync(binaryLeft));
+            return Expression.Constant(node.Right);
+        });
+
+        var result = await Task.WhenAll(firstTask.Value, secondTask.Value);
+
+        return node.NodeType switch
+        {
+            ExpressionType.Add => Expression.Add(result[0], result[1]),
+            ExpressionType.Subtract => Expression.Subtract(result[0], result[1]),
+            ExpressionType.Multiply => Expression.Multiply(result[0], result[1]),
+            ExpressionType.Divide => result[1].Value!.Equals(0) 
+                ? throw new Exception(MathErrorMessager.DivisionByZero) 
+                : Expression.Divide(result[0], result[1]),
+            _ => throw new InvalidOperationException("Operation not supported")
+        };
+    }
+}
