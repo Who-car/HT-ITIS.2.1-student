1c9e899cccc195ef49b0fed8324aedb6bfed4434
Who-car <aysulu2645@gmail.com>
2023-11-15 14:22:46 +0300

    ДЗ 9. Задание на доп.баллы: F# клиент, основанный на AsyncEitherBuilder

---
+++ Homework9/AsyncEitherClient/AsyncEitherBuilder.fs
@@ -0,0 +1,21 @@
+﻿module AsyncEitherClient.AsyncEitherBuilder
+
+open Microsoft.FSharp.Control
+
+type AsyncEither<'a, 'b> = Async<Result<'a, 'b>>
+
+type AsyncEitherBuilder() =
+    member this.Bind(m, k) =
+        async {
+            let! res = m
+            match res with
+            | Ok value -> return! k value
+            | Error error -> return Error error
+        }
+
+    member this.Return(value) :Async<Result<'a, 'b>> =
+        async {
+            return Ok value
+        }
+
+let asyncEither = AsyncEitherBuilder()
---
+++ Homework9/AsyncEitherClient/Program.fs
@@ -0,0 +1,38 @@
+﻿module AsyncEitherClient.App
+
+open AsyncEitherClient.AsyncEitherBuilder
+open System.Net.Http
+
+let runAsyncEither (action: Async<Result<'a, 'b>>) =
+    async {
+        let! res = action
+        return res
+    } 
+
+let sendRequestAsync (url: string) =
+    async {
+        let httpClient = new HttpClient()
+        try
+            let! response = httpClient.GetAsync(url) |> Async.AwaitTask
+            return Ok (response.Content.ReadAsStringAsync().Result)
+        with
+           | ex -> return Error ex.Message
+    }
+
+let test =
+    asyncEither {
+            let! response1 = sendRequestAsync "https://api.example.com/endpoint1"
+            let! response2 = sendRequestAsync "https://api.example.com/endpoint1"
+            return $"First response result: {response1};\nSecond response result: {response2}."
+        }
+
+let main =
+    async {
+        let! result = test |> runAsyncEither
+
+        match result with
+        | Ok value -> printfn "Result: %s" value
+        | Error error -> printfn "Error: %s" error
+    }
+
+main |> Async.RunSynchronously
