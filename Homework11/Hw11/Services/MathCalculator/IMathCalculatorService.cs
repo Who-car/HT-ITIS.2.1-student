a44043e34a94fff0e5e3c2a69e3316f350bfb2f6
Who-car <aysulu2645@gmail.com>
2023-11-25 23:33:55 +0300

    ДЗ 11. Expression Visitor переписан с использованием паттерна multiple dispatch

--- Homework11/Hw11/Services/MathCalculator/IMathCalculatorService.cs
+++ Homework11/Hw11/Services/MathCalculator/IMathCalculatorService.cs
@@ -1,6 +1,8 @@
+using Hw11.Dto;
+
 namespace Hw11.Services.MathCalculator;
 
 public interface IMathCalculatorService
-{ 
-    Task<double> CalculateMathExpressionAsync(string? expression);
+{
+    public Task<double> CalculateMathExpressionAsync(string? expression);
 }
--- Homework11/Hw11/Services/MathCalculator/MathCalculatorService.cs
+++ Homework11/Hw11/Services/MathCalculator/MathCalculatorService.cs
@@ -1,9 +1,16 @@
+using System.Linq.Expressions;
+using Hw11.Dto;
+
 namespace Hw11.Services.MathCalculator;
 
 public class MathCalculatorService : IMathCalculatorService
 {
     public async Task<double> CalculateMathExpressionAsync(string? expression)
     {
-        throw new NotImplementedException();
+        MathExpressionValidatorService.ValidateExpression(expression);
+        dynamic tree = MathParserService.ParseExpression(expression!);
+        var result = await ExpressionVisitorDispatcher.ExpressionVisitorDispatcher.Visit(tree);
+            
+        return result;
     }
 }
